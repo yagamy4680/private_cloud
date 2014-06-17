dc1578e449175dc895b348b3c0770dd6e2a6b976
yagamy4680 <yagamy@gmail.com>
2014-06-17 21:43:27 +0800

    install nodejs from  Chris Lea's repo

--- scripts/install/04_nodejs.sh
+++ scripts/install/04_nodejs.sh
@@ -1,45 +1,9 @@
 #!/bin/bash
 
-NODE_VERSION=$(apt-cache show nodejs | grep "^Version" | cut --delimiter=' ' -f2 | cut --delimiter='~' -f1)
-
-if [ "" != "${NODE_VERSION}" ]; then
-	# Use apt repository to install the compiled nodejs.
-	#
-	echo "nodejs v${NODE_VERSION} is available on apt repository"
-	apt-get install -y -q nodejs
-else
-	# Install nodejs from source
-	# 
-	NODE_VERSION="0.10.29"
-	NODE_FILE=$(which node)
-
-	if [ "" != "${NODE_FILE}" ]; then
-		echo "nodejs ($(node --version)) was installed at ${NODE_FILE}"
-		exit 0
-	fi
-
-	CURRENT=$(pwd)
-
-	# Download sources and compile.
-	#
-	NODE_URL="https://github.com/joyent/node/archive/v${NODE_VERSION}.tar.gz"
-	wget -O /tmp/node-${NODE_VERSION}.tar.gz ${NODE_URL}
-	cd /tmp
-	tar -xvf node-${NODE_VERSION}.tar.gz
-	cd node-${NODE_VERSION}
-	./configure --prefix=/opt/node
-	make -j$(nproc)
-	make install
-
-	# Setup environment path for nodejs execution.
-	#
-	PATHES=$(cat /etc/environment | grep -v "^#" | grep "^PATH" | cut --delimiter='"' -f2)
-	PATHES="${PATHES}:/opt/node/bin"
-	sed -i 's/^PATH.*$//g' /etc/environment
-	echo "PATH=\"${PATHES}\"" >> /etc/environment
-	export PATH=${PATH}:/opt/node/bin
-	cd ${CURRENT}
-fi
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:chris-lea/node.js
+apt-get update -q
+apt-get install -y -q python-software-properties python g++ make nodejs
 
 npm install -g LiveScript
 npm install -g coffee-script
