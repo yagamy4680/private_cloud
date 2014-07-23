5553fae20cc4449982a0b4e611511c16b116fd16
yagamy4680 <yagamy@gmail.com>
2014-07-24 01:54:12 +0800

    Update 04_nodejs.sh

--- scripts/install/04_nodejs.sh
+++ scripts/install/04_nodejs.sh
@@ -2,10 +2,10 @@
 
 # Refer to https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager
 #
-apt-get install -y software-properties-common
+apt-get install -y software-properties-common python-software-properties python
 add-apt-repository -y ppa:chris-lea/node.js
 apt-get update -q
-apt-get install -y -q python-software-properties python g++ make nodejs
+apt-get install -y -q g++ make nodejs
 
 npm install -g LiveScript
 npm install -g coffee-script
